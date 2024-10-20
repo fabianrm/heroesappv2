e14b10226b54f8dbb51ce6095ee78b77c407e82f
Fabian Ramirez <fabianrm@gmail.com>
2024-10-19 22:05:16 -0500

    servicios, pipes, carga de imagen

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -16,4 +16,15 @@ export class HeroesService {
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this._baseUrl}/heroes`);
   }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${this._baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError(err => of(undefined)) //Retorna un observable (of) si da error
+      )
+  }
+
+
+
+
 }
